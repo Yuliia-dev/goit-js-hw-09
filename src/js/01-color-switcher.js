11804c9530f2bf01801b49b060cf63be0f7446b4
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2021-11-04 15:30:59 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startEl = document.querySelector('button[data-start]');
+const stopEl = document.querySelector('button[data-stop]');
 
+startEl.addEventListener('click', startGenerisColor);
+stopEl.addEventListener('click', stopGenerisColor);
+let timerId = null;
+let isActive = false;
+
+function startGenerisColor() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  const bodyEl = document.querySelector('body');
+  bodyEl.classList.add('color');
+  const colorBody = document.querySelector('.color');
+
+  timerId = setInterval(() => {
+    colorBody.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopGenerisColor() {
+  isActive = false;
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
